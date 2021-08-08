99d96c2dc7c6510c54219b3061fe673ab68fa980
CREATE TAMILZAN <88332002+CREATETAMIZAN@users.noreply.github.com>
2021-08-08 11:19:12 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,7 +4,7 @@ from pyrogram import Client, emoji, filters
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
 
-from utils import get_search_results
+from utils import get_search_results, is_subscribed
 
 
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
